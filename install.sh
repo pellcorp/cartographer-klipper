7334cac8388309bff93ad6d18a22b6ec8ddbd53e
Jason Pell <jason@pellcorp.com>
2024-07-26 12:00:21 +1000

    disable the install.sh copying over the cartographer.py as its integrated into klipper repo now

--- install.sh
+++ install.sh
@@ -6,31 +6,33 @@
 # Copyright (C) 2023 Beacon <beacon3d.com>
 # This file may be distributed under the terms of the GNU GPLv3 license.
 
-PYTHON_EXEC="$KENV/bin/python"
+# PYTHON_EXEC="$KENV/bin/python"
 
-BKDIR=/usr/data/cartographer-klipper
+# BKDIR=/usr/data/cartographer-klipper
 
-if [ -d "/usr/share/klipper" ] && [ -d "/usr/share/klippy-env" ]; then
-  KDIR="/usr/share/klipper"
-  KENV="/usr/share/klippy-env"
-elif [ -d "/usr/data/klipper" ] && [ -d "/usr/data/klippy-venv" ]; then
-  KDIR="/usr/data/klipper"
-  KENV="/usr/data/klippy-venv"
-else
-  echo "Cartographer: klipper or klippy env doesn't exist"
-  exit 1
-fi
+# if [ -d "/usr/share/klipper" ] && [ -d "/usr/share/klippy-env" ]; then
+#   KDIR="/usr/share/klipper"
+#   KENV="/usr/share/klippy-env"
+# elif [ -d "/usr/data/klipper" ] && [ -d "/usr/data/klippy-venv" ]; then
+#   KDIR="/usr/data/klipper"
+#   KENV="/usr/data/klippy-venv"
+# else
+#   echo "Cartographer: klipper or klippy env doesn't exist"
+#   exit 1
+# fi
 
-# update link to cartographer.py & idm.py
-echo "Cartographer: linking modules into klipper"
-for file in cartographer.py; do
-    if [ -e "${KDIR}/klippy/extras/${file}" ]; then
-        rm "${KDIR}/klippy/extras/${file}"
-    fi
-    ln -s "${BKDIR}/${file}" "${KDIR}/klippy/extras/${file}"
-    if ! grep -q "klippy/extras/${file}" "${KDIR}/.git/info/exclude"; then
-        echo "klippy/extras/${file}" >> "${KDIR}/.git/info/exclude"
-    fi
-done
+# # update link to cartographer.py & idm.py
+# echo "Cartographer: linking modules into klipper"
+# for file in cartographer.py; do
+#     if [ -e "${KDIR}/klippy/extras/${file}" ]; then
+#         rm "${KDIR}/klippy/extras/${file}"
+#     fi
+#     ln -s "${BKDIR}/${file}" "${KDIR}/klippy/extras/${file}"
+#     if ! grep -q "klippy/extras/${file}" "${KDIR}/.git/info/exclude"; then
+#         echo "klippy/extras/${file}" >> "${KDIR}/.git/info/exclude"
+#     fi
+# done
 
-echo "Cartographer Probe: installation successful."
+# echo "Cartographer Probe: installation successful."
+
+echo "Cartographer klipper is now integrated into simple-af klipper fork"
